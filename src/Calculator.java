1045b59ee3027858f4c5cb3bd588c18bc7d9a4a3
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-10-19 11:16:32 +0530

    team1 teja has commited

---
+++ src/Calculator.java
@@ -0,0 +1,25 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public static double add(double n1,double n2) {
+			//
+			
+		}
+		public static double sub(double n1,double n2) {
+			//
+			
+		}
+		public static double mul(double n1,double n2) {
+			//
+			
+		}
+		public static double div(double n1,double n2) {
+			//
+			
+		}
+
+	}
+
+}
